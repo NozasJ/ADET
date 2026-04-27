7ae1c5b9b3a3ac743876a7f3df51f69fa842cdba
NozasJ <johnbenedictsazonedu@gmail.com>
2026-04-27 16:49:10 +0800

    Create index.php

---
+++ fa1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $variable = "Hello World";
+    ?>
+    <div class="container">
+        <div class="header">
+            <h1>PHP Activities</h1>
+            <p><?= $variable; ?></p>
+        </div>
+    </div>
+
+</body>
+</html>
