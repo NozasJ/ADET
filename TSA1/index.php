34c821770a1424913f3abc094d87c084b3931562
NozasJ <johnbenedictsazonedu@gmail.com>
2026-05-11 18:57:49 +0800

    Create index.php

---
+++ TSA1/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+body{
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background-color: #f0f2f5;
+    font-family: sans-serif;
+}
+.container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #fff;
+    padding: 40px;
+    margin: 50px auto;
+    width: 90%;
+    max-width: 800px;
+    box-shadow: 0 10px 25px rgba(0,0,0,0.1);
+    border: 1px solid #eee;
+    border-radius: 2px;
+}
+button{
+    background-color: #007BFF;
+    color: white;
+    width: 200px;
+    height: 50px;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 16px;
+    padding: 10px 20px;
+    margin: 20px;
+}
+button:hover{
+    background-color: #0056b3;
+}
+
+</style>
+<body>
+    <div class = "container">
+        <h1> TECHNICAL SUMMATIVE 1 PROJECTS </h1>
+        <a href = "StudReg.php"><button> 1. Student Registration</button></a>
+        <a href = "MultTable.php"><button> 2. Multiplication Table</button></a>
+    </div>
+</body>
+</html>
