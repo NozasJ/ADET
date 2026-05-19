918a6612735f25d1354a67edd350d9f72a27d7a6
NozasJ <johnbenedictsazonedu@gmail.com>
2026-05-19 15:32:13 +0800

    Create top.php

---
+++ TSA2/include/top.php
@@ -0,0 +1,21 @@
+             <?php
+            $fname = "John Benedict I. Sazon";
+            $Occupation = "Undergraduate Student";
+            $Course = "BS Information Technology - Cybersecurity";
+            $desc ="An undergraduate student majoring in Bachelor of Science and Information Technology with Specialization in Cybersecity, equipped with a wide range of skill set in technologies including C++, HTML, CSS, Adobe. With a strong adaptability and seeks opportunities to expand technical expertise.";
+            $univ = "Far Eastern University - Institute of Technology";    
+            
+           
+            ?>
+            <div class="header">
+            <h1><?= $fname; ?></h1>
+            <p><?= $Occupation; ?></p>
+        </div>
+       <div class="content">
+            <div class = "line"></div>
+                <p style = "font-size: 20pt; font-family: 'Times New Roman', Times, serif; color: rgb(5, 5, 5); font-weight: Bold;padding: 10px;margin: 0;">Profile</p>
+            <div class = "line"></div>
+            <div class = description>
+                <p><?= $desc; ?></p>
+            </div>
+        </div>
