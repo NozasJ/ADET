0857b2003ef0f0e2aba08df5292e0dab208941cb
NozasJ <johnbenedictsazonedu@gmail.com>
2026-05-19 13:54:52 +0800

    Create yssa.php

---
+++ fa4/include/yssa.php
@@ -0,0 +1,6 @@
+            <div class="card">
+                <h1> Yssa </h1>
+                    <img src = 'include/a1.jpg' style = "width: 90px; height: 90px; border-radius: 50%; object-fit: cover; border: 3px solid white;">
+                    <p> Zodiac Sign: Gemini</p>
+                    <p> Since the first few weeks college she's always been with me. It was awkard at first, but we became friends to best friends to lovers just like in the movies!  </p>
+            </div>
