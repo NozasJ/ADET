0494931b8a535016e908ce1c0ad9db4f3e6648e7
NozasJ <johnbenedictsazonedu@gmail.com>
2026-05-19 13:54:42 +0800

    Create france.php

---
+++ fa4/include/france.php
@@ -0,0 +1,6 @@
+            <div class="card">
+                <h1> France </h1>
+                    <img src = 'include/f1.jpg' style = "width: 90px; height: 90px; border-radius: 50%; object-fit: cover; border: 3px solid white;">
+                    <p> Zodiac Sign: Virgo</p>
+                    <p> We became close because of the pandemic era. During the days when I was isolated inside the house, he is one of the few that I got to play with.</p>
+            </div>
