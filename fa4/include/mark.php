5c40351cf45a7745e78a28f9c6e16677d0e2d88d
NozasJ <johnbenedictsazonedu@gmail.com>
2026-05-19 13:54:49 +0800

    Create mark.php

---
+++ fa4/include/mark.php
@@ -0,0 +1,6 @@
+            <div class="card">
+                <h1> Mark </h1>
+                    <img src = 'include/m1.jpg' style = "width: 90px; height: 90px; border-radius: 50%; object-fit: cover; border: 3px solid white;">
+                    <p> Zodiac Sign: Cancer</p>
+                    <p> One of the few close friends that I have since the first few weeks of college. We always get along well when it comes with online games.</p>
+            </div>
