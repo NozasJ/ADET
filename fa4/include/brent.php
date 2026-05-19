f3d43f6f9713ec7854b13e71d286823f6e52eb32
NozasJ <johnbenedictsazonedu@gmail.com>
2026-05-19 13:54:38 +0800

    Create brent.php

---
+++ fa4/include/brent.php
@@ -0,0 +1,6 @@
+            <div class="card">
+                <h1> Brent </h1>
+                    <img src = 'include/b1.jpg' style = "width: 90px; height: 90px; border-radius: 50%; object-fit: cover; border: 3px solid white;">
+                    <p> Zodiac Sign: Capricorn</p>
+                    <p>He was my friend since elementary school! We became friend because I bumped into him in grade 1 and we were brought to the clinic.</p>
+            </div>
