6b29d25f34d48487f8009bdc5bfa4888ee9283e1
NozasJ <johnbenedictsazonedu@gmail.com>
2026-05-19 15:32:06 +0800

    Create contact.php

---
+++ TSA2/include/contact.php
@@ -0,0 +1,11 @@
+             
+             <?php $gmail = "Gmail: johnbenedictsazonedu@gmail.com";
+            $femail = "FEU Email: 202412140@fit.edu.ph";
+            $cnum = "Contact Number: 09931239007";?>
+             
+             <p style = "font-size: 20pt; font-family: 'Times New Roman', Times, serif; color: rgb(5, 5, 5); font-weight: Bold;padding: 10px;margin: 0;">Contact Information</p>
+            <div class = "line"></div>
+                <p><?= $gmail; ?></p>
+                <p><?= $femail; ?></p>
+                <p><?= $cnum; ?></p>
+            </div>
