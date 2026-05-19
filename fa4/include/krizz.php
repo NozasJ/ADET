30ec5a6e9aac04956779071b58aa7dc1dba23776
NozasJ <johnbenedictsazonedu@gmail.com>
2026-05-19 13:54:45 +0800

    Create krizz.php

---
+++ fa4/include/krizz.php
@@ -0,0 +1,6 @@
+            <div class="card">
+                <h1> Krizz </h1>
+                    <img src = 'include/k1.jpg' style = "width: 90px; height: 90px; border-radius: 50%; object-fit: cover; border: 3px solid white;">
+                    <p> Zodiac Sign: Taurus</p>
+                    <p> I was scared of her because she doesn't communicate with me that often and that made me feel like she doesn't want to be friends with me. Turns out, we were just both scared of each other.</p>
+            </div>
