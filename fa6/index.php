9ea99b98b8145dd4f45d77bbb3523528fb085f47
NozasJ <johnbenedictsazonedu@gmail.com>
2026-06-22 18:35:53 +0800

    Create index.php

---
+++ fa6/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <a href="registration.php" class="menucard">
+            <div style="font-size: 36px;">🐶</div>
+            <div style="font-size: 16px; font-weight: 500;">Register a Dog</div>
+            <div style="font-size: 12px; color: #888; font-family: Arial, sans-serif;">Add a new dog!</div>
+        </a>
+
+        <a href="list.php" class="menucard">
+            <div style="font-size: 36px;">📋</div>
+            <div style="font-size: 16px; font-weight: 500;">List of Dogs</div>
+            <div style="font-size: 12px; color: #888; font-family: Arial, sans-serif;">View all registered dogs</div>
+        </a>
+    </div>
+</body>
+</html>
